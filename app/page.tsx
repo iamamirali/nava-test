15bdbed1559e367aae69e9f210737de3467e680d
AmirAli Shabani <iamamirali.sh@gmail.com>
2026-08-02 12:45:21 +0330

    feat: add custom theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
 export default function Home() {
   return (
-    <main>
-      <p>سلام دنیا</p>
+    <main className="grow flex flex-col items-center justify-center">
+      <h1 className="bg-primary-50 text-primary text-center text-4xl font-bold p-4">
+        سلام دنیا
+      </h1>
+      <p className="grow bg-primary-100 p-4">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+        quisquam illo dolorum earum praesentium sint iste dicta? Expedita
+        quaerat modi praesentium, nihil dolore magni nostrum accusantium eos
+        iure, velit fugiat est, facilis suscipit labore impedit quia amet odio
+        quisquam iusto aliquid debitis nesciunt fuga nisi eum. Consequatur
+        molestiae quidem quibusdam!
+      </p>
     </main>
   );
 }
