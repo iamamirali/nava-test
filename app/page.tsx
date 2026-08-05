8ed3fb70ae2ab199e54eee6b1ad1fdb103d9c95a
AmirAli Shabani <iamamirali.sh@gmail.com>
2026-08-05 15:00:17 +0330

    feat: add docker configs

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
   return (
     <main className="grow flex flex-col items-center justify-center">
-      <h1 className="bg-primary-50 text-primary text-center text-4xl font-bold p-4">
+      <h1 className="text-primary text-center text-4xl font-bold p-4">
         سلام دنیا
       </h1>
-      <p className="grow bg-primary-100 p-4">
+      <p className="grow bg-primary-50 p-4">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
         quisquam illo dolorum earum praesentium sint iste dicta? Expedita
         quaerat modi praesentium, nihil dolore magni nostrum accusantium eos
