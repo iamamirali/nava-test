df60290d6ef497eb9d96a493ee103cea6f69c84d
AmirAli Shabani <iamamirali.sh@gmail.com>
2026-08-09 12:54:39 +0330

    style: set rtl dir for main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fa" className={`${customFont.className} h-full antialiased`}>
+    <html lang="fa" dir="rtl" className={`${customFont.className} h-full antialiased`}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
