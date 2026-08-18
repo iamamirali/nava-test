b35a5a29082432f6ee6ee5dd49584ee6a20ac7c1
AmirAli Shabani <iamamirali.sh@gmail.com>
2026-08-18 18:19:02 +0330

    fix: update next & auth config suitable for production

--- lib/auth.ts
+++ lib/auth.ts
@@ -13,7 +13,6 @@ export class CredentialsSigninError extends CredentialsSignin {
 
 export const { auth, signIn, signOut, handlers } = NextAuth({
   ...authConfig,
-  trustHost: true,
   providers: [
     Credentials({
       credentials: {
